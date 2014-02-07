383ba36e070c50c4c8bacbd0d58a2b0ed2cd429e
Philipp Schmitt <philipp@schmitt.co>
2014-02-07 16:21:11 +0100

    Add calculus preview windows

--- src/io/lxl/android/stupidCalculator/activity/MainActivity.java
+++ src/io/lxl/android/stupidCalculator/activity/MainActivity.java
@@ -2,9 +2,16 @@ package io.lxl.android.stupidCalculator.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import io.lxl.android.stupidCalculator.R;
+import io.lxl.android.stupidCalculator.model.GestureObject;
 
 public class MainActivity extends Activity {
+
+    private TextView mCalculusView;
+    private List<GestureObject> mObjectList;
+
+
     /**
      * Called when the activity is first created.
      */
@@ -12,5 +19,11 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Retain views
+        mCalculusView = (TextView) findViewById(R.id.text_calculus);
+
+        // Init list
+        mObjectList = new ArrayList<GestureObject>();
     }
 }
